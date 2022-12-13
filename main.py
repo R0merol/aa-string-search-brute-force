29e766e02e32556da7861172c005ec04f763e4ae
R0merol <farisfrontiers@gmail.com>
2022-12-14 00:14:05 +0700

    Tambah algoritma brute force dan teks tambahan

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import json
+import textwrap
 
-def baca_json() -> dict:  # pake txt juga bisa kali
+
+def baca_json() -> dict:
     with open("teks.json", "r") as file:
         return json.load(file)
-    
 
-def cek_file_json():  # Gk penting
+
+def cek_file_json():  # cek apakah ada file teks.json atau tidak
     try:
         with open("teks.json", "r") as file:
             file.close()
@@ -14,27 +16,49 @@ def cek_file_json():  # Gk penting
         with open("data.json", "w") as f:
             json.dump(default, f, indent=2)
 
+
 def input_teks():
-    print("-"*10+" Input Teks "+"-"*10)
+    print(f"{' Input Teks ':=^100}")
     judul = input("Judul teks: ")
     teks = input("Tempelkan teks di sini: ")
 
     cek_file_json()
-    
+
     with open("teks.json", "r") as file:
         teks_json = json.load(file)
 
     teks_json[judul] = teks
     teks_json = dict(sorted(teks_json.items()))
-    
+
     with open("teks.json", "w") as file:
         json.dump(teks_json, file, indent=2)
 
-def brute_force(teks: str):
-    pass
+
+def brute_force(pola: str, teks: str):
+    M = len(pola)
+    N = len(teks)
+    jumlah_pola = 0
+
+    # Looping pola[] satu per satu
+    for i in range(N - M + 1):
+        j = 0
+
+        # Untuk index i, cek pola yang cocok
+        while(j < M):
+            if (teks[i + j] != pola[j]):
+                break
+            j += 1
+
+        if (j == M):
+            print(f'Kata "{pola}" ditemukan pada indeks: {i}')
+            jumlah_pola += 1
+
+    if jumlah_pola == 0:
+        print(f'Kata "{pola}" tidak ditemukan pada teks tersebut')
+
 
 def pencarian_string():
-    print("-"*10+" Pencarian String "+"-"*10)
+    print(f"{' Pencarian String ':=^100}")
     while True:
         try:
             print("Pilih judul teks:")
@@ -44,44 +68,47 @@ def pencarian_string():
                 print(f"{i+1}. {judul}")
                 list_judul.append(judul)
             index = int(input(">>> Pilihan: ")) - 1
-            print("-"*20)
+            print("-" * 100)
             print("Judul: " + list_judul[index])
             print("Teks:")
             teks = teks_json[list_judul[index]]
-            print(teks)
-            print("-"*20)
-            brute_force(teks)
+            print(textwrap.fill(teks, width=100))
+            print("-" * 100)
+            pola = input(">>> Masukkan kata yang ingin dicari: ")
+            brute_force(pola, teks)
             break
         except IndexError:
-            print("Pilihan tidak ada, silahkan input kembali")
+            print(f"{' Pilihan tidak ada, silahkan input kembali ':!^100}")
             continue
         except ValueError:
-            print("Pilihan harus berbentuk nomor, silahkan input kembali")
+            print(f"{' Pilihan harus berbentuk nomor, silahkan input kembali ':!^100}")
             continue
+
 
 def apakah_ingin_mengulang() -> bool:
     print("Apakah anda ingin mengulang program?")
     jawaban = input(">>> Jawaban (y): ")
     return True if jawaban.lower() == "y" else False
 
+
 def main():
-    print("-" * 10 + " Pencarian String menggunakan Algoritma Brute-Force " + "-" * 10)
+    print(f"{' Pencarian String menggunakan Algoritma Brute-Force ':=^100}")
     while True:
         print("Pilih opsi tersebut:")
-        print("1. Input teks")
-        print("2. Pencarian string")
+        print("1. Pencarian string")
+        print("2. Input teks")
         pilihan = input(">>> Pilihan Anda: ")
-        if pilihan=="1":
-            input_teks()
-        elif pilihan=="2":
+        if pilihan == "1":
             pencarian_string()
+        elif pilihan == "2":
+            input_teks()
         else:
-            print("Pilihan salah, silahkan input kembali")
-            print("#"*20)
+            print(f"{' Pilihan salah, silahkan input kembali ':!^100}")
             continue
         if apakah_ingin_mengulang():
             continue
         break
+
 
 if __name__ == '__main__':
     main()
